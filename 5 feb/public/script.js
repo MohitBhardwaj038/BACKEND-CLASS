e9255acac023b38ebe963b3f2862c26637ba3b7c
Mohit Bhardwwaj <mohitsharma93165@gmail.com>
2025-02-22 18:26:04 +0530

    UPDATE

--- 5 feb/public/script.js
+++
@@ -1,5 +0,0 @@
-let users = [
-  { username: "admin", age: 20, email: "mohit@gmail.com" },
-  { username: "admin2", age: 20, email: "mohitsharma@gmail.com" },
-  { username: "admin3", age: 20, email: "mohitbhardwaj@gmail.com" },
-];
